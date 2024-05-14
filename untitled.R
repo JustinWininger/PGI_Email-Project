55995a0156400bc18c11fe0abc7cfaf16791b94e
jwhine1 <justinrules2005@outlook.com>
2024-05-14 15:42:54 -0400

    idk

---
+++ untitled.R
@@ -0,0 +1,3 @@
+library("groundhog")
+pkgs <- c('Stat2Data', 'tidyverse', 'mosaic', 'ggformula', 'Lock5Data', 'tinytex', 'car', 'leaps', 'HH', 'rvest', 'dplyr', 'readr', 'ggplot2', 'tidyr', 'readxl', 'esquisse', 'kableExtra', 'ggformula')
+groundhog.library(pkgs, '2023-11-29', tolerate.R.version='4.4.0')
